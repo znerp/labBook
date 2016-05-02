346fbc4fd9700da0fe1d3526bba8b2bcb9b0eb4e
sw463 <sw463@cam.ac.uk>
2016-05-02 13:33:54 +0200

    update setup labbook

--- tests/testthat/test_labBook_setup.R
+++ tests/testthat/test_labBook_setup.R
@@ -1,6 +1,8 @@
 
 setwd("../test_labBook/LabBook/")
+rm(list=ls())
 
-labBook_setup("Test", "~/Desktop/LabBook/labbook_code/test_labBook")
+unlink("~/Desktop/LabBook/labbook_code/test_labBook/LabBook", recursive = TRUE)
+labBook_setup("Test", "~/Desktop/LabBook/labbook_code/test_labBook", project_name = "My first project")
 
 labBook_newProject("My first project")
