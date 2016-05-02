d1a226f2bc7d2cc677d0bdfa68cd6d9baf38f354
sw463 <sw463@cam.ac.uk>
2016-05-02 10:44:21 +0200

    Remove specific functions

--- R/Rstudio_addins.R
+++ R/Rstudio_addins.R
@@ -17,18 +17,3 @@ open_webpage <- function(){
   system(paste("open",html_path))
 }
 
-## Function for updating antibody landscapes package to latest version
-update_AbLandscapes <- function(){
-  devtools::install_github(repo                 = "sw463/ab-landscapes",
-                           auth_token           = "0e26b2eb12d2d6c0c3290a3476719d986b306dec",
-                           subdir               = "AbLandscapes",
-                           type                 = "source",
-                           upgrade_dependencies = FALSE)
-}
-
-## Function for updating labbook to latest version
-update_LabBook_pkg <- function(){
-  remove.packages("labBook")
-  install.packages("../labbook_code/labBook_0.1.0.tar.gz", repos = NULL, type="source")
-}
-
