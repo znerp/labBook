9738f2a8776fe794ebb558155496c22f2e6621fd
sw463 <sw463@cam.ac.uk>
2016-05-02 16:37:28 +0200

    Add edit code function

--- R/Rstudio_addins.R
+++ R/Rstudio_addins.R
@@ -4,8 +4,17 @@ edit_webpage <- function(){
   doc_info  <- rstudioapi::getActiveDocumentContext()
   code_path <- doc_info$path
   html_path <- gsub("\\.R", "\\.html", code_path)
-  html_path <- gsub("code/", "", html_path)
+  html_path <- gsub("/code/", "/", html_path)
   file.edit(html_path)
+}
+
+## Function for editing webpage associated with current code document
+edit_code <- function(){
+  doc_info  <- rstudioapi::getActiveDocumentContext()
+  html_path <- doc_info$path
+  code_path <- gsub("\\.html", "\\.R", html_path)
+  code_path <- gsub("(^.*/)(.*?)$", "\\1code/\\2", code_path)
+  file.edit(code_path)
 }
 
 ## Function for viewing webpage associated with current code document
@@ -13,7 +22,7 @@ open_webpage <- function(){
   doc_info  <- rstudioapi::getActiveDocumentContext()
   code_path <- doc_info$path
   html_path <- gsub("\\.R", "\\.html", code_path)
-  html_path <- gsub("code/", "", html_path)
+  html_path <- gsub("/code/", "/", html_path)
   system(paste("open",html_path))
 }
 
