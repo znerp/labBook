466a4c0f03ecfcd8c56d653810ee3fbcf7147669
sw463 <sw463@cam.ac.uk>
2016-05-02 11:42:44 +0200

    Use testthat

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(labBook)
+
+test_check("labBook")
---
+++ tests/testthat/test_labBook_setup.R
@@ -0,0 +1,6 @@
+
+setwd("../test_labBook/LabBook/")
+
+labBook_setup("Test", "~/Desktop/LabBook/labbook_code/test_labBook")
+
+labBook_newProject("My first project")
